633d7c0604147daf43f9a9a030d7723deb864a92
calee14 <caplee1000@gmail.com>
2018-08-20 18:27:49 -0700

    saved training in a pickle

--- MLPractice/regression.py
+++ MLPractice/regression.py
@@ -6,6 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 from matplotlib import style
+import pickle
 
 style.use('ggplot')
 
@@ -37,12 +38,17 @@ X = X[:-forecast_out]
 
 df.dropna(inplace=True)
 y = np.array(df['label'])
-y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 clf = LinearRegression()
 clf.fit(X_train, y_train)
+with open('linearregression.pickle', 'wb') as f:
+	pickle.dump(clf, f)
+	
+pickle_in = open('linearregression.pickle', 'rb')
+clf = pickle.load(pickle_in)
+
 accuracy = clf.score(X_test, y_test)
 
 # print(accuracy)
