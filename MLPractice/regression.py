5e93dd200d59227b0b6ef0236da83f003c60e8c9
calee14 <caplee1000@gmail.com>
2018-08-21 20:54:31 -0700

    fixed some errors

--- MLPractice/regression.py
+++ MLPractice/regression.py
@@ -12,20 +12,21 @@ style.use('ggplot')
 
 today = datetime.date.today()
 
-df = quandl.get('WIKI/GOOGL', end_date=str(today))
+df = quandl.get('WIKI/GOOG', end_date=str(today))
 
 # create features
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume',]]
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
 
+#            price       x          x              x
 df = df[['Adj. Close','HL_PCT','PCT_change','Adj. Volume']]
 
 forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 
 # predict 10 percent of the dataframe
-forecast_out = int(math.ceil(0.01*len(df)))
+forecast_out = int(math.ceil(0.1*len(df)))
 print(forecast_out)
 
 df['label'] = df[forecast_col].shift(-forecast_out)
