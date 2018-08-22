3a820de875bc04df54cc2154c602f6fccee62fd9
calee14 <caplee1000@gmail.com>
2018-08-21 20:53:24 -0700

    added testing assumption code

--- MLPractice/bestfitslope.py
+++ MLPractice/bestfitslope.py
@@ -9,6 +9,19 @@ style.use('fivethirtyeight')
 xs = np.array([1,2,3,4,5,6], dtype=np.float64)
 ys = np.array([5,4,6,5,6,7], dtype=np.float64)
 
+def create_dataset(hm, variance, step=2, correlation=False):
+	val = 1
+	ys = []
+	for i in range(hm):
+		y = val + random.randrange(-variance, variance)
+		ys.append(y)
+		if correlation and correlation == 'pos':
+			val += step
+		elif correlation and correlation == 'neg':
+			val -= step
+	xs = [i for i in range(len(ys))]
+	return np.array(xs, dtype=np.float64), np.array(ys, dtype=np.float64)
+
 def best_fit_slope_and_intercept(xs, ys):
 	m = ( ((mean(xs) * mean(ys)) - mean(xs*ys)) / 
 		  ((mean(xs)**2) - mean(xs**2)) )
@@ -23,6 +36,8 @@ def coefficient_of_determination(ys_orig, ys_line):
 	squared_error_regr = squared_error(ys_orig, ys_line)
 	squared_error_y_mean = squared_error(ys_orig, y_mean_line)
 	return 1 - (squared_error_regr / squared_error_y_mean)
+
+xs, ys = create_dataset(40, 10, 2, correlation='pos')
 
 m,b = best_fit_slope_and_intercept(xs, ys)
 
