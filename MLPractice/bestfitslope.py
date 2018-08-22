46c35dca8442a64ce140c2c956f078aa6c51f25c
calee14 <caplee1000@gmail.com>
2018-08-21 20:33:44 -0700

    added accuracy function to regression

--- MLPractice/bestfitslope.py
+++ MLPractice/bestfitslope.py
@@ -2,6 +2,7 @@ from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
+import random
 
 style.use('fivethirtyeight')
 
@@ -14,12 +15,24 @@ def best_fit_slope_and_intercept(xs, ys):
 	b = mean(ys) - m*mean(xs)
 	return m, b
 
+def squared_error(ys_orig, ys_line):
+	return sum((ys_line-ys_orig)**2)
+
+def coefficient_of_determination(ys_orig, ys_line):
+	y_mean_line = [mean(ys_orig) for y in ys_orig]
+	squared_error_regr = squared_error(ys_orig, ys_line)
+	squared_error_y_mean = squared_error(ys_orig, y_mean_line)
+	return 1 - (squared_error_regr / squared_error_y_mean)
+
 m,b = best_fit_slope_and_intercept(xs, ys)
 
 regression_line = [(m*x)+b for x in xs]
 
 predict_x = 8
 predict_y = (m*predict_x)+b
+
+r_squared = coefficient_of_determination(ys, regression_line)
+print(r_squared)
 
 plt.scatter(xs, ys)
 plt.scatter(predict_x, predict_y, color='g')
