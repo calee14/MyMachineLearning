82484bac7cac9790913c442f7ee467c0c66c8d2b
calee14 <caplee1000@gmail.com>
2018-08-23 18:50:41 -0700

    finished implementation of k nearest neighbors

--- MLPractice/knearestneighbors.py
+++ MLPractice/knearestneighbors.py
@@ -17,3 +17,9 @@ clf.fit(X_train, y_train)
 
 accuracy = clf.score(X_test, y_test)
 print(accuracy)
+
+example_measures = np.array([4,2,1,1,1,2,3,2,1])
+example_measures = example_measures.reshape(len(example_measures), -1)
+
+prediction = clf.predict(example_measures)
+print(prediction)
