516c7591b3948de844be6f5303977a8fa2de8215
calee14 <caplee1000@gmail.com>
2018-09-09 16:04:34 -0700

    can now dynamically shift

--- MLPractice/meanshiftscratch.py
+++ MLPractice/meanshiftscratch.py
@@ -21,28 +21,63 @@ X = np.array([[1, 2],
 colors = 10*['g','r','c','b','k','o']
 
 class Mean_Shift:
-	def __init__(self, radius=4):
+	def __init__(self, radius=None, radius_norm_step=100):
 		self.radius = radius
+		self.radius_norm_step = radius_norm_step
 
 	def fit(self, data):
+
+		if self.radius == None:
+			all_data_centroid = np.average(data, axis=0)
+			all_data_norm = np.linalg.norm(all_data_centroid)
+			self.radius = all_data_norm / self.radius_norm_step
+
 		centroids = {}
 
 		for i in range(len(data)):
 			centroids[i] = data[i]
+
+		weights = [i for i in range(self.radius_norm_step)][::-1]
 
 		while True:
 			new_centroids = []
 			for i in centroids:
 				in_bandwidth = []
 				centroid = centroids[i]
+
+				weights = [i for i in range(self.radius_norm_step)][::-1] # don't need this anymore
+
 				for featureset in data:
-					if np.linalg.norm(featureset-centroid) < self.radius:
-						in_bandwidth.append(featureset)
+					distance = np.linalg.norm(featureset-centroid)
+					if distance == 0:
+						distance = 0.000000001
+
+					weight_index = int(distance/self.radius)
+					if weight_index > self.radius_norm_step-1:
+						weight_index = self.radius_norm_step-1
+					to_add = (weights[weight_index]**2)*[featureset]
+					in_bandwidth += to_add
 
 				new_centroid = np.average(in_bandwidth, axis=0)
 				new_centroids.append(tuple(new_centroid))
 
 			uniques = sorted(list(set(new_centroids)))
+
+			to_pop = []
+
+			for i in uniques:
+				for ii in uniques:
+					if i == ii:
+						pass
+					elif np.linalg.norm(np.array(i)-np.array(ii)) <= self.radius:
+						to_pop.append(ii)
+						break
+
+			for i in to_pop:
+				try:
+					uniques.remove(i)
+				except:
+					pass
 
 			prev_centroids = dict(centroids)
 
