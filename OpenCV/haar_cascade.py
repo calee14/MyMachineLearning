e85d80a562af58f0d8e7ea7a083ccbfe34c2bd59
calee14 <caplee1000@gmail.com>
2018-10-30 22:03:34 -0700

    tried to use a hand cascade found off the internet but was good

--- OpenCV/haar_cascade.py
+++ OpenCV/haar_cascade.py
@@ -1,15 +1,19 @@
 import cv2
 import numpy as np
 
-face_cascade = cv2.CascadeClassifier('/Users/cap1/machinelearning/opencv/haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-eye_cascade = cv2.CascadeClassifier('/Users/cap1/machinelearning/opencv/haarcascade_eye.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
+hand_cascade = cv2.CascadeClassifier('aGest.xml')
 cap = cv2.VideoCapture(0)
 
 while True:
 	ret, img = cap.read()
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	hands = hand_cascade.detectMultiScale(gray)
+	for (x,y,w,h) in hands:
+		cv2.rectangle(img, (x,y), (x+w,y+h), (255,255,0), 2)
 	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 	for (x,y,w,h) in faces:
 		cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
