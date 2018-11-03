aae0a755587d16716a3a0d283abf8311ebc4e3eb
calee14 <caplee1000@gmail.com>
2018-11-02 20:55:11 -0700

    resizing image

--- OpenCV/multi_scale_template.py
+++ OpenCV/multi_scale_template.py
@@ -20,3 +20,23 @@ template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
 template = cv2.Canny(template, 50, 200)
 (tH, tW) = template.shape[:2]
 cv2.imshow("Template", template)
+
+# loop over the images to find the template in
+for imagePath in glob.glob(args["images"] + "/*.jpg"):
+	# load the image, convert it to grayscale, and initialize the
+	# book keeping variable to keep track of the matched region
+	image = cv2.imread(imagePath)
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	found = None
+
+	# loop over the scales of the image
+	for scale in np.linespace(0.2, 1.0, 20)[::-1]:
+		# resize the image according to the scale, and keep track
+		# of the ratio of the resizing
+		resized = imutils.resize(gray, width=int(gray.shape[1] * scale))
+		r = gray.shape[1] / float(resized.shape[1])
+
+		# if the resized image is smaller than the template, then break
+		# from the loop
+		if resized.shape[0] < tH or resized.shape[1] < tW:
+			break
