5da0ac46ec5234ca47e9a06c15433a4e6ed04978
calee14 <caplee1000@gmail.com>
2018-11-02 21:10:36 -0700

    template matching

--- OpenCV/multi_scale_template.py
+++ OpenCV/multi_scale_template.py
@@ -40,3 +40,34 @@ for imagePath in glob.glob(args["images"] + "/*.jpg"):
 		# from the loop
 		if resized.shape[0] < tH or resized.shape[1] < tW:
 			break
+
+		# detect edges in the resized, grayscale image and apply template
+		# matching to find the template in the imge
+		edged = cv2.Canny(resized, 50, 200)
+		result = cv2.matchTemplate(edged, template, cv2.TM_CCOEFF)
+		(_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
+
+		# check to see if the iteration should be visualized
+		if args.get("visulalize", False):
+			# draw a bounding box around the detected region
+			clone = np.dstack([edged, edged, edged])
+			cv2.rectangle(clone, (maxLoc[0], maxLoc[1]),
+				(maxLoc[0] + tW, maxLoc[1] + tH), (0, 0, 255), 2)
+			cv2.imshow("Visualize", clone)
+			cv2.waitKey(0)
+
+		if found is None or maxVal > found[0]:
+			found = (maxVal, maxLoc, r)
+
+	# unpack the book keeping variable and compute the (x, y) coordinates
+	# of the bounding box based on the resized ratio
+	(_, maxLoc, r) = found
+	(startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
+	(endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
+
+	# draw a bounding bo around the detected result and display the image
+	cv2.rectangle(image, (startX, startY), (endX, endY), (0,0,255), 2)
+	cv2.imshow("Image", image)
+	cv2.waitKey(0)
+# To run enter this command:
+# python multi_scale_template.py --template cod_logo.png --images images
