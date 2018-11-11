0b96dd94dab1cc615b2a4a728a88af187642ebfb
calee14 <caplee1000@gmail.com>
2018-11-11 15:56:56 -0800

    got live video feed to work with multi scaling

--- OpenCV/handrecognition/handrecognition.py
+++ OpenCV/handrecognition/handrecognition.py
@@ -21,6 +21,62 @@ template = cv2.Canny(template, 50, 200)
 (tH, tW) = template.shape[:2]
 cv2.imshow("Template", template)
 
+cap = cv2.VideoCapture(0)
+
+while True:
+	# load the image, convert it to grayscale, and initialize the
+	# book keeping variable to keep track of the matched region
+	ret, image = cap.read()
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	gray = cv2.Canny(gray, 50, 200)
+	cv2.imshow('Frame', gray)
+	found = None
+
+	# loop over the scales of the image
+	for scale in np.linspace(0.2, 1.0, 20)[::-1]:
+		# resize the image according to the scale, and keep track
+		# of the ratio of the resizing
+		resized = imutils.resize(gray, width=int(gray.shape[1] * scale))
+		r = gray.shape[1] / float(resized.shape[1])
+
+		# if the resized image is smaller than the template, then break
+		# from the loop
+		if resized.shape[0] < tH or resized.shape[1] < tW:
+			break
+
+		# detect edges in the resized, grayscale image and apply template
+		# matching to find the template in the imge
+		edged = cv2.Canny(resized, 50, 200)
+		result = cv2.matchTemplate(edged, template, cv2.TM_CCOEFF)
+		(_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
+
+		# check to see if the iteration should be visualized
+		# if args.get("visulalize", False):
+		# 	# draw a bounding box around the detected region
+		# 	clone = np.dstack([edged, edged, edged])
+		# 	cv2.rectangle(clone, (maxLoc[0], maxLoc[1]),
+		# 		(maxLoc[0] + tW, maxLoc[1] + tH), (0, 0, 255), 2)
+		# 	cv2.imshow("Visualize", clone)
+		# 	cv2.waitKey(0)
+
+		if found is None or maxVal > found[0]:
+			found = (maxVal, maxLoc, r)
+
+	# unpack the book keeping variable and compute the (x, y) coordinates
+	# of the bounding box based on the resized ratio
+	(_, maxLoc, r) = found
+	(startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
+	(endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
+
+	# draw a bounding bo around the detected result and display the image
+	cv2.rectangle(image, (startX, startY), (endX, endY), (0,0,255), 2)
+	cv2.imshow("Image", image)
+	# cv2.waitKey(0)
+
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+'''
 # loop over the images to find the template in
 for imagePath in glob.glob(args["images"] + "/*.png"):
 	# load the image, convert it to grayscale, and initialize the
@@ -71,3 +127,4 @@ for imagePath in glob.glob(args["images"] + "/*.png"):
 	cv2.waitKey(0)
 # To run enter this command:
 # python handrecognition.py --template cod_logo.png --images images
+'''
